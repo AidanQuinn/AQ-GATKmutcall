8087d3241f85d278cb18969a72cfc5c8fd46e780
Aidan Quinn <quinn0219@gmail.com>
2018-09-13 13:52:59 -0400

    Update 2_cleanAlignment.sh

--- 2_cleanAlignment.sh
+++ 2_cleanAlignment.sh
@@ -1,23 +1,37 @@
- #!/bin/bash
+#!/bin/bash
 
- # Vars
+# Vars
+fastq_dir="/data/scratch/20180912_CTCL_Exome_Analysis/fastq"
+samples="/data/scratch/20180912_CTCL_Exome_Analysis/scripts/samples.txt"
+bwa_index="/data/shares/ref/bwa/GRCh38/GRCh38p12"
+align_dir="/data/scratch/20180912_CTCL_Exome_Analysis/aligned"
+out_dir="/data/scratch/20180912_CTCL_Exome_Analysis/aligned"
 
- fastq_dir="/data/scratch/20180912_CTCL_Exome_Analysis/fastq"
- samples="/data/scratch/20180912_CTCL_Exome_Analysis/scripts/samples.txt"
- bwa_index="/data/shares/ref/bwa/GRCh38/GRCh38p12"
- align_dir="/data/scratch/20180912_CTCL_Exome_Analysis/aligned"
- out_dir="/data/scratch/20180912_CTCL_Exome_Analysis/aligned"
+for i in `cat $samples`; do
+    date
+    echo "Sorting $i"
+    gatk SortSam \
+        --INPUT ${align_dir}/${i}.sam \
+        --OUTPUT ${out_dir}/${i}_sorted.bam \
+        --SORT_ORDER coordinate
+    echo "done."; echo ""
+done
 
- for i in `cat $samples`; do
-     date
-     echo "Sorting $i"
-     gatk SortSam \
-         --INPUT ${align_dir}/${i}.sam \
-         --OUTPUT ${out_dir}/${i}_sorted.bam \
-         --SORT_ORDER coordinate
-     echo "done."; echo ""
- done
+# [Optionally] remove sam files
+for i in `cat $samples`; do
+    if [ ${out_dir}/${i}_sorted.bam -s ]; then
+        rm -f ${align_dir}/${i}.sam
+    fi
+done
 
+# Run MarkDuplicates
+for i in `cat $samples`; do
+    date
+    echo "Marking duplicates on $i"
+    gatk MarkDuplicates \
+        --INPUT ${align_dir}/${i}_sorted.bam \
+        --OUTPUT --OUTPUT ${out_dir}/${i}_sorted_dupMarked.bam
+    echo "done."; echo ""
+done
 
-
- echo "Complete."
+echo "Complete."
